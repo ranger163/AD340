dbadde86993503705e483264ebaa0012f6817fe5
ranger163 <ahmed.nassar163@gmail.com>
2020-10-25 13:27:30 +0200

    Adding logic to MainActivity.kt

--- app/src/main/java/me/inassar/ad340/MainActivity.kt
+++ app/src/main/java/me/inassar/ad340/MainActivity.kt
@@ -7,6 +7,9 @@ import kotlinx.android.synthetic.main.activity_main.*
 
 
 class MainActivity : AppCompatActivity() {
+
+    private val forecastRepository = ForecastRepository()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -16,8 +19,14 @@ class MainActivity : AppCompatActivity() {
             if (zipcode.length != 5) {
                 Toast.makeText(this, "zipCode entry error", Toast.LENGTH_SHORT).show()
             } else {
-                Toast.makeText(this, zipcode, Toast.LENGTH_SHORT).show()
+                forecastRepository.loadForecast(zipcode)
             }
         }
+
+        forecastRepository.weeklyForecast.observe(this, ::updateUi)
+    }
+
+    private fun updateUi(forecastItems: List<DailyForecast>) {
+        Toast.makeText(this, forecastItems.toString(), Toast.LENGTH_LONG).show()
     }
 }
