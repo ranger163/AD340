637ed2518f8f0874b9934af8328c09ffc6501d2b
ranger163 <ahmed.nassar163@gmail.com>
2020-10-25 13:16:11 +0200

    Adding logic to ForecastRepository.kt

--- app/src/main/java/me/inassar/ad340/ForecastRepository.kt
+++ app/src/main/java/me/inassar/ad340/ForecastRepository.kt
@@ -1,4 +1,25 @@
 package me.inassar.ad340
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import java.math.RoundingMode
+import kotlin.random.Random
+
 class ForecastRepository {
+
+    private val _weeklyForecast = MutableLiveData<List<DailyForecast>>()
+    val weeklyForecast: LiveData<List<DailyForecast>>
+        get() = _weeklyForecast
+
+    fun loadForecast(zipCode: String) {
+        val randomValues = List(7) {
+            Random.nextFloat().rem(100).times(100).toBigDecimal()
+                .setScale(2, RoundingMode.UP)
+                .toFloat()
+        }
+        val forecastItems = randomValues.map { temperature ->
+            DailyForecast(temperature, "Partly Cloudy")
+        }
+        _weeklyForecast.value = forecastItems
+    }
 }
