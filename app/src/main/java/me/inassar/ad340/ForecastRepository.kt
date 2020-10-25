3555d1b6ff4914b950dd49a3b3f5a898fc6e8e46
ranger163 <ahmed.nassar163@gmail.com>
2020-10-25 15:37:56 +0200

    Adding more descriptive temperature descriptions

--- app/src/main/java/me/inassar/ad340/ForecastRepository.kt
+++ app/src/main/java/me/inassar/ad340/ForecastRepository.kt
@@ -18,8 +18,22 @@ class ForecastRepository {
                 .toFloat()
         }
         val forecastItems = randomValues.map { temperature ->
-            DailyForecast(temperature, "Partly Cloudy")
+            DailyForecast(temperature, getTempDesc(temperature))
         }
         _weeklyForecast.value = forecastItems
+    }
+
+    private fun getTempDesc(temp: Float): String {
+        return when (temp) {
+            in Float.MIN_VALUE.rangeTo(0f) -> "Anything below 0 doesn't make sense."
+            in 0f.rangeTo(32f) -> "Way too cold."
+            in 32f.rangeTo(55f) -> "Colder than I would prefer."
+            in 55f.rangeTo(65f) -> "Getting better."
+            in 65f.rangeTo(80f) -> "That's the sweet spot!"
+            in 80f.rangeTo(90f) -> "Getting a little worm."
+            in 90f.rangeTo(100f) -> "Dude, turn on the A/C."
+            in 100f.rangeTo(Float.MAX_VALUE) -> "OK you are in hell now :D"
+            else -> "Doesn't compute"
+        }
     }
 }
