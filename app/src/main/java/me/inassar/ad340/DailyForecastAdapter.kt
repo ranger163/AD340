cce9150caae5b0dab36a9334d04e3a5a52dcc040
ranger163 <ahmed.nassar163@gmail.com>
2020-10-25 14:53:54 +0200

    Adding logic to DailyForecastAdapter.kt

---
+++ app/src/main/java/me/inassar/ad340/DailyForecastAdapter.kt
@@ -0,0 +1,48 @@
+package me.inassar.ad340
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import kotlinx.android.synthetic.main.item_daily_forecast.view.*
+
+class DailyForecastAdapter : ListAdapter<DailyForecast, DailyForecastViewHolder>(DIFF_CONFIG) {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): DailyForecastViewHolder {
+        return DailyForecastViewHolder(
+            LayoutInflater.from(parent.context).inflate(R.layout.item_daily_forecast, parent, false)
+        )
+    }
+
+    override fun onBindViewHolder(holder: DailyForecastViewHolder, position: Int) {
+        holder.bindUi(getItem(position))
+    }
+
+    companion object {
+        val DIFF_CONFIG = object : DiffUtil.ItemCallback<DailyForecast>() {
+            override fun areItemsTheSame(oldItem: DailyForecast, newItem: DailyForecast): Boolean {
+                return oldItem === newItem
+            }
+
+            override fun areContentsTheSame(
+                oldItem: DailyForecast,
+                newItem: DailyForecast
+            ): Boolean {
+                return oldItem == newItem
+            }
+
+        }
+    }
+
+}
+
+class DailyForecastViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+    fun bindUi(dailyForecast: DailyForecast) {
+        itemView.apply {
+            forecastTempTv.text = dailyForecast.temp.toString()
+            forecastDescTv.text = dailyForecast.description
+        }
+    }
+}
