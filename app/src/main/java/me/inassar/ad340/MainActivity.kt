c2df7991437ea41a3a58c9271f4771a84eff0ac3
ranger163 <ahmed.nassar163@gmail.com>
2020-10-25 12:24:50 +0200

    Adding button click

--- app/src/main/java/me/inassar/ad340/MainActivity.kt
+++ app/src/main/java/me/inassar/ad340/MainActivity.kt
@@ -1,12 +1,23 @@
 package me.inassar.ad340
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        enterButton.setOnClickListener {
+            val zipcode: String = zipcodeEditText.text.toString()
+
+            if (zipcode.length != 5) {
+                Toast.makeText(this, "zipCode entry error", Toast.LENGTH_SHORT).show()
+            } else {
+                Toast.makeText(this, zipcode, Toast.LENGTH_SHORT).show()
+            }
+        }
     }
 }
